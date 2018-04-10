2f7067f66ccc311c79a4cbee0580d597cbaad71c
alezana <azanardi@ethz.ch>
2018-04-10 16:02:08 +0200

    rough estimate done

--- MATLAB/gokartSYSID/torque_to_speed_sysid.m
+++ MATLAB/gokartSYSID/torque_to_speed_sysid.m
@@ -5,28 +5,35 @@ columns_name = ["time", "torque_speed_control_sx",...
     "rear_wheel_rate_rx",    "tangent_speed",...
     "angular_rate",    "angular_rate_proportional_to_steering_angle"];
 data_raw = csvread("pursuit_20180307T154859.csv");
-data = struct();
+data_struct = struct();
 for i=1:length(columns_name)
     field = convertStringsToChars(columns_name(i));
-    data(1).(field) = data_raw(:, i);
+    data_struct(1).(field) = data_raw(:, i);
 end
 
 %% first ignorant approach fft -> etf
-N = length(data_raw);
-Ts = mean(diff(data(1).time));
+t = data_struct(1).time;
+N = length(t);
+Ts = mean(diff(t));
 Fs = 1/Ts;
 f = Fs*(0:(N/2))/N;
+u = data_struct(1).torque_speed_control_rx;
+y = data_struct(1).tangent_speed;
 
-U = fft(data(1).torque_speed_control_rx);
-Y = fft(data(1).tangent_speed);
+U = fft(u);
+Y = fft(y);
 omega = (2*pi/N)*(0:N-1)';
 idx = find(omega > 0 & omega < pi);
+
+figure(1);
+subplot(2,1,1)
 loglog(omega(idx),abs(U(idx)))
+subplot(2,1,2)
 loglog(omega(idx),abs(Y(idx)))
 Gest = Y./U; % ETFE estimate
 
 %% Plots
-figure(1);
+figure(2);
 
 subplot(2,2,1)
 loglog(omega(idx),abs(Gest(idx)))
@@ -39,3 +46,24 @@ semilogx(omega(idx),angle(Gest(idx)))
 % 
 % subplot(2,2,4)
 % plot(f,angle(Gest(idx)))
+
+% https://it.mathworks.com/help/ident/ref/etfe.html
+
+%% Controller PI
+z = tf('z',Ts);
+Ki = 20; % [Nm/rad]
+Kp = 20; % [Nm*s/rad]
+
+C_pi = Kp + Ki*1/z;
+
+
+%% With sysid toolbox
+data = iddata(y,u,Ts);
+ge = etfe(data);
+gs = spa(data);
+figure(3);
+bode(ge,gs);
+
+%% FFT check
+figure(4);
+plot(omega(idx), abs(U(idx)));
