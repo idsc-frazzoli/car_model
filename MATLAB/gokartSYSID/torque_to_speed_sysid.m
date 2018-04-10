c1ebf253f6568d00cd93c11632c83923a99bc086
alezana <azanardi@ethz.ch>
2018-04-10 15:20:00 +0200

    rough etfe

--- MATLAB/gokartSYSID/torque_to_speed_sysid.m
+++ MATLAB/gokartSYSID/torque_to_speed_sysid.m
@@ -6,7 +6,36 @@ columns_name = ["time", "torque_speed_control_sx",...
     "angular_rate",    "angular_rate_proportional_to_steering_angle"];
 data_raw = csvread("pursuit_20180307T154859.csv");
 data = struct();
-for i=1:size(columns_name,2)
+for i=1:length(columns_name)
     field = convertStringsToChars(columns_name(i));
     data(1).(field) = data_raw(:, i);
 end
+
+%% first ignorant approach fft -> etf
+N = length(data_raw);
+Ts = mean(diff(data(1).time));
+Fs = 1/Ts;
+f = Fs*(0:(N/2))/N;
+
+U = fft(data(1).torque_speed_control_rx);
+Y = fft(data(1).tangent_speed);
+omega = (2*pi/N)*(0:N-1)';
+idx = find(omega > 0 & omega < pi);
+loglog(omega(idx),abs(U(idx)))
+loglog(omega(idx),abs(Y(idx)))
+Gest = Y./U; % ETFE estimate
+
+%% Plots
+figure(1);
+
+subplot(2,2,1)
+loglog(omega(idx),abs(Gest(idx)))
+
+subplot(2,2,2)
+semilogx(omega(idx),angle(Gest(idx)))
+
+% subplot(2,2,3)
+% semilogy(f,abs(Gest(idx)))
+% 
+% subplot(2,2,4)
+% plot(f,angle(Gest(idx)))
