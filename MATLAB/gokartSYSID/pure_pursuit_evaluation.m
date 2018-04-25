14390c266e241598781d8daad16b89471ec59af7
alezana <azanardi@ethz.ch>
2018-04-25 15:34:41 +0200

    small stuff

--- MATLAB/gokartSYSID/pure_pursuit_evaluation.m
+++ MATLAB/gokartSYSID/pure_pursuit_evaluation.m
@@ -8,7 +8,7 @@ columns_name = ["time", "torque_speed_control_l",...
     "rear_wheel_rate_r",    "tangent_speed",...
     "angular_rate",    "angular_rate_proportional_to_steering_angle",...
     "joystick_head_avg","pure_pursuit_speed" ];
-data_raw = csvread(name3);
+data_raw = csvread(name2);
 data_struct = struct();
 for i=1:length(columns_name)
     field = convertStringsToChars(columns_name(i));
@@ -20,15 +20,18 @@ t = data_struct(1).time;
 
 figure(1);
 %subplot(2,1,1)
-plot(t, data_struct(1).torque_speed_control_r, 'b');
+h1 = plot(t, data_struct(1).torque_speed_control_r, 'b');
 hold on;
-plot(t, data_struct(1).rear_wheel_rate_r*20, 'g');
+h2 = plot(t, data_struct(1).rear_wheel_rate_r*20, 'g');
 grid on
 %plot(t, data_struct(1).joystick_head_avg*1000, 'r');
-plot(t, data_struct(1).pure_pursuit_speed*1000, 'k');
+h3 = plot(t, data_struct(1).pure_pursuit_speed*1000, 'k');
 
-legend("torque speed control", "rear wheel rate",...
-    "pure pursuit speed")
+set([h1 h2 h3],'LineWidth',1.2)
+lgd = legend("u - torque speed control", "y - rear wheel rate",...
+    "r - pure pursuit speed");
+lgd.FontSize = 16;
+lgd.Location = "northeast";
 % %"joystick head avg"
     
 title("torque speed control")
